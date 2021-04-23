be43c4afe1b047f40ded4c660965305a7c955899
Vladymyr Radionov <radionovdon@gmail.com>
2021-04-23 16:29:41 +0300

    animations

--- ddstgroup/src/js/main.js
+++ ddstgroup/src/js/main.js
@@ -1,17 +1,44 @@
 'use strict';
 
 //declare
-const percentContainer = document.querySelectorAll('.progress__text');
+const percentContainer = document.querySelectorAll('.progress__text'),
+    prog1 = document.getElementById('prog1'),
+    prog2 = document.getElementById('prog2'),
+    prog3 = document.getElementById('prog3'),
+    progress = document.querySelector('.progress'),
+    progDone = document.querySelector('.progress--done'),
+    report = document.querySelector('.report');
 let percent = 0;
 
-//get percents
-let percStatus = setInterval( function() {
-        percent++;
-        percentContainer.forEach(
-            it => it.innerHTML = `${percent}`
-        )
-        if (percent == 100) {
-            clearInterval(percStatus);
-        }
-}, 10);
 
+
+//animate  show/hide content
+let animateElem = function (elem, condition) {
+    elem.classList.add(condition);
+    setTimeout(function () {
+        elem.classList.add('show');
+    }, 100);
+}
+
+//get percents
+let percStatus = setInterval(function () {
+    percent++;
+    percentContainer.forEach(
+        it => it.innerHTML = `${percent}%`
+    )
+    if (percent == 33) {
+        animateElem(prog1, 'flex');
+    }
+    if (percent == 66) {
+        animateElem(prog2, 'flex');
+    }
+    if (percent == 100) {
+        animateElem(prog3, 'flex');
+        clearInterval(percStatus);
+        setTimeout(function () {
+            progress.style.display = 'none';
+            animateElem(progDone, 'block');
+            animateElem(report, 'block');
+        }, 1000);
+    }
+}, 20);
